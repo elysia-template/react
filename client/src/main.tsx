d01843fbc436f077b5f57140870844447a0b94ed
wetoon <phiraphantanan@gmail.com>
2024-11-13 13:42:10 +0700

    router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -17,8 +17,16 @@ const router = createBrowserRouter([
         path: "*",
         element: <NotFound />
     }
-])
+], {
+    future: {
+        v7_skipActionErrorRevalidation:true,
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_relativeSplatPath: true
+    }
+})
 
 createRoot( document.getElementById( 'application-container-wrapper' ) as HTMLElement ).render(
-    <RouterProvider router={ router } />
+    <RouterProvider future={{ v7_startTransition:true }} router={ router } />
 )
